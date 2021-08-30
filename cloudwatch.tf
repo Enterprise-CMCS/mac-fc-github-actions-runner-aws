12b4bb2b0db2ac43fb4bf0d0d19cfce2efaab347
Reid Lewis <reid@truss.works>
2021-08-30 15:04:05 -0700

    more narrowly scoping cloudwatch log access

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -61,5 +61,14 @@ data "aws_iam_policy_document" "cloudwatch_logs_allow_kms" {
       "kms:Describe*"
     ]
     resources = ["*"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+
+      values = [
+        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/ecs/${var.environment}/github-runner",
+      ]
+    }
   }
 }
