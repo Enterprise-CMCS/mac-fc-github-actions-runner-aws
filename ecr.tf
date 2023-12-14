b8ab334f6aed421fe23f446fb0378faac1ac4bda
Leslie Klein <leslie@corbalt.com>
2023-12-14 17:59:44 -0500

    import ECR in MACBIS Shared DSO Dev account into terraform

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = "= 1.5.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["037370603820"]
+
+}
+
 data "aws_organizations_organization" "cmsgov" {}
 
 resource "aws_ecr_repository" "github_actions_runner" {
