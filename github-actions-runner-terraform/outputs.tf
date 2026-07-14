61913309dd8f2247b41f9397fc0bb344ad0530b7
Leslie Klein <leslie@corbalt.com>
2026-07-14 17:06:27 -0400

    remove unused Terraform modules

--- github-actions-runner-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecs_service_arn" {
-  description = "The ARN of the self-hosted runner ECS service"
-  value       = aws_ecs_service.actions-runner.id
-}
-
-output "ecs_security_group_id" {
-  description = "The security group ID of the self-hosted runner ECS service"
-  value       = aws_security_group.ecs_sg.id
-}
